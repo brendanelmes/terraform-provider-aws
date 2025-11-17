0457e7e94bc4e3d492207d889aa1aea1f45470b3
Brendan <brendanelmes@improbable.io>
2025-11-17 17:12:04 +0000

    slo name change requires replacement

--- internal/service/applicationsignals/service_level_objective.go
+++ internal/service/applicationsignals/service_level_objective.go
@@ -21,6 +21,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
@@ -80,6 +82,9 @@ func (r *resourceServiceLevelObjective) Schema(ctx context.Context, req resource
 			},
 			names.AttrName: schema.StringAttribute{
 				Required: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 		},
 		Blocks: map[string]schema.Block{
